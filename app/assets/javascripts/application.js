e2bc0b60f2516c22df9e186a18801f7bfb8cbb4e
eugene masuda <eugene0531masuda@yahoo.co.jp>
2020-07-09 11:40:48 +0900

    heroku js 設定

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,9 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+//= require jquery
+//= require jquery.flexslider-min
+//= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
